4265f4368b97d28f46ef5a038e8f1115d546e07c
charly-dizier <charly.dizier@gmail.com>
2024-05-05 12:15:26 +0200

    front footer

--- front/src/utils/Router.jsx
+++ front/src/utils/Router.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 //Composant importer
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 //Pages importer
 import Home from "../pages/Home";
@@ -20,6 +21,7 @@ function Router() {
                     <Route exact path="/" element={<Home />} />
                     <Route exact path="/login" element={<Login />} />
                 </Routes>
+                <Footer />
             </BrowserRouter>
         </React.StrictMode>
     ) 
